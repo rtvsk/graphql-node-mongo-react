21d11f3648ee24ef34f035c520b40595365fca95
Codemecodeyou <codemecodeyou@gmail.com>
2019-09-30 15:19:06 +0300

    added basic schema+types

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -6,9 +8,35 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Hello!');
-})
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantic Cooking', 'Sleeping', 'All-Night coding'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  }),
+);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
